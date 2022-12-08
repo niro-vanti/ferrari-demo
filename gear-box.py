a7848f9330a23a3a4a8a3b2b4d5dc8faccde1b4b
niro <niro@vanti-analytics.com>
2022-12-08 13:57:27 +0200

    sdf

--- gear-box.py
+++ gear-box.py
@@ -12,40 +12,6 @@ from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-mesh_size = .02
-margin = 0.25
-
-# Load and split data
-X, y = make_moons(noise=0.3, random_state=0)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y.astype(str), test_size=0.25, random_state=0)
-
-# Create a mesh grid on which we will run our model
-x_min, x_max = X[:, 0].min() - margin, X[:, 0].max() + margin
-y_min, y_max = X[:, 1].min() - margin, X[:, 1].max() + margin
-xrange = np.arange(x_min, x_max, mesh_size)
-yrange = np.arange(y_min, y_max, mesh_size)
-xx, yy = np.meshgrid(xrange, yrange)
-
-# Create classifier, run predictions on grid
-clf = KNeighborsClassifier(15, weights='uniform')
-clf.fit(X, y)
-Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
-Z = Z.reshape(xx.shape)
-
-
-# Plot the figure
-fig = go.Figure(data=[
-    go.Contour(
-        x=xrange,
-        y=yrange,
-        z=Z,
-        colorscale='RdBu'
-    )
-])
-fig.show()
-st.write(fig)
-
 
 page_title = "GearBox Anomaly Detection App"
 page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
@@ -109,6 +75,46 @@ with st.sidebar:
 st.image('assets/Images/Vanti - Main Logo@4x copy.png', width=200)
 st.title(page_title)
 st.text(' ')
+
+
+
+mesh_size = .02
+margin = 0.25
+
+# Load and split data
+X, y = make_moons(noise=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y.astype(str), test_size=0.25, random_state=0)
+
+# Create a mesh grid on which we will run our model
+x_min, x_max = X[:, 0].min() - margin, X[:, 0].max() + margin
+y_min, y_max = X[:, 1].min() - margin, X[:, 1].max() + margin
+xrange = np.arange(x_min, x_max, mesh_size)
+yrange = np.arange(y_min, y_max, mesh_size)
+xx, yy = np.meshgrid(xrange, yrange)
+
+# Create classifier, run predictions on grid
+clf = KNeighborsClassifier(15, weights='uniform')
+clf.fit(X, y)
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+Z = Z.reshape(xx.shape)
+
+
+# Plot the figure
+fig = go.Figure(data=[
+    go.Contour(
+        x=xrange,
+        y=yrange,
+        z=Z,
+        colorscale='RdBu'
+    )
+])
+fig.show()
+st.write(fig)
+
+
+
+
 
 # st.image('assets/Images/car-pano-1.jpg')
 
