46520fc445948964cb4193400ca1f9c38031a8b8
niro <niro@vanti-analytics.com>
2022-12-14 15:17:06 +0200

    commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/niro/Documents/GitHub/Real-Time-Anomaly/venv"
+set -gx VIRTUAL_ENV "/Users/niro/Documents/GitHub/ferrari-demo/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
