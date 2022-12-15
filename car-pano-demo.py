615673a5b1ac287becc47f1e20b8815e1b251b9f
niro <niro@vanti-analytics.com>
2022-12-15 18:31:24 +0200

    deploy for demo app

--- car-pano-demo.py
+++ car-pano-demo.py
@@ -31,21 +31,17 @@ color_scale = alt.Scale(range=['#FAFA37', '#52de97', '#c9c9c9'])
 N = 146
 zoom_names = []
 defect_list = {}
+
+alerts = pd.DataFrame()
+
+
 for file in os.listdir('assets/Images'):
     if '_zoom' in file:
         it = file.split('_')[0]
         dif = file.split('_')[-1].split('.')[0]
         defect_list[it] = dif
-# print(defect_list)
 
 
-
-def get_class():
-    classes = ['scratch','dirt','discoloration']
-    ind = np.random.randint(0,2,1)[0]
-    return classes[ind]
-
-alerts = pd.DataFrame()
 
 
 with st.sidebar:
@@ -66,13 +62,11 @@ with st.sidebar:
 
 
 
-# st.image('assets/Images/Vanti - Main Logo@4x copy.png', width=200)
 st.title(page_title)
 st.text(' ')
 st.image('assets/Images/ferrari-cropped.png')
 
 c1, c2 = st.columns(2)
-# stream = c1.button('Start Injection mode')
 
 pl = st.empty()
 p2 = st.empty()
@@ -87,14 +81,8 @@ if stream:
         stream = False
 
     for i in range(1000):
-
         with pl.container():
-
-            image = Image.open('assets/Images/' + str(i % N) + '_rect.png')
-            data = np.asarray(image)
-            # covMat = np.array(covMat, dtype=float)
-            fig = px.imshow(data).update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
-            st.write(fig)
+            st.image('assets/Images/' + str(i % N) + '_rect.png')
 
             defect = defect_list[str(i % N)]
             print(i, defect)
@@ -103,31 +91,24 @@ if stream:
                 st.success('no defect!')
                 is_error = False
             else:
-                st.error('found a defect - '+defect)
-                image = Image.open('assets/Images/' + str(i % N) + '_zoom_' + defect + '.png')
-                data = np.asarray(image)
-                err_fig = px.imshow(data).update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
-                # cls = get_class()
                 is_error = True
                 q = pd.DataFrame({
                     'section': [i % N],
                     'defect': [defect],
                 })
                 alerts = pd.concat([alerts, q], axis=0, ignore_index=True)
-            # with st.expander('defect list'):
-            #     sbc = st.empty()
-            #     with sbc.container():
-            #         st.write(alerts)
         if is_error:
             with st.expander(defect+'  ::  defect-alert zoom-in @ section' + str(i % N)):
-                st.write(err_fig)
+                st.image('assets/Images/' + str(i % N) + '_zoom_' + defect + '.png')
 
 hide_streamlit_style = """
             <style>
             footer {visibility: hidden;}
-            primary="#52de97"
-            backgroundColor = "#FFFFFF"
-            textColor = "#000000"
+            root {
+                primary="#52de97";
+                backgroundColor = "#FFFFFF";
+                textColor = "#000000";
+            }
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
