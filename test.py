139543c7ca5712e0a2671edc765dd1a92e0391cb
niro <niro@vanti-analytics.com>
2023-02-28 16:43:43 +0200

    asdfsdf

--- test.py
+++ test.py
@@ -671,8 +671,9 @@ def cpc(cpc_stream):
 
 
 def roadmap():
-    source = 'https://sharing.clickup.com/5712158/b/h/5ea8y-1402/c3da18542cfe989'
-    source = 'https://sharing.clickup.com/5712158/l/h/5ea8y-2262/ec42799aa27a3e3'
+    # source = 'https://sharing.clickup.com/5712158/b/h/5ea8y-1402/c3da18542cfe989'
+    # source = 'https://sharing.clickup.com/5712158/l/h/5ea8y-2262/ec42799aa27a3e3'
+    source = 'https://sharing.clickup.com/5712158/l/h/5ea8y-2362/621790f9c8c8d11'
     st.components.v1.iframe(src=source,
                             width=1500,
                             height=900,
