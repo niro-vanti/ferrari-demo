f1761c4a4d05baa778e164d409333396478a6b39
niro <niro@vanti-analytics.com>
2023-05-01 18:05:38 +0300

    initial video demo

--- assets/specific_demos/conveyor_belt_demo.py
+++ assets/specific_demos/conveyor_belt_demo.py
@@ -4,3 +4,11 @@ import numpy as np
 import plotly.express as px
 
 
+def cb_demo(si_stream, stop_stream, files):
+    st.title('Video Based Object Detection')
+    st.subheader('running detection on conveyor belt')
+    st.write('---------------------------------------------------------')
+
+    orange_video = files[0]
+
+    st.video(orange_video)
