d111071f3587f7b69ea23ba5258ce0daf2aa3927
niro <niro@vanti-analytics.com>
2023-05-30 17:57:59 +0300

    asdf

--- assets/specific_demos/diego.py
+++ assets/specific_demos/diego.py
@@ -56,8 +56,8 @@ def diego(diego_strem, stop_stream, files):
             rr2 = regr.score(X_test, y_test)
             # st.text(f'niro {')
 
-            df['model'] = [i+np.random.randn()*s/3 for i in df[y]]
-            df['model'].fillna(0, inplace=True)
+            # df['model'] = [i+np.random.randn()*s/3 for i in df[y]]
+            # df['model'].fillna(0, inplace=True)
             y_pred = regr.predict(X)
             df['model'] = y_pred
             q = pd.DataFrame(df[[y,'model']])
@@ -82,55 +82,59 @@ def diego(diego_strem, stop_stream, files):
     with st.expander('Calculator'):
         # st.text(len(y_col))
         if len(y_col)>0:
-            # st.text('y_col')
-            df_corr = df.corr()
-            df_feats = pd.DataFrame(np.abs(df_corr[y_col]))
-            df_feats.sort_values(by=[y_col], inplace=True, ascending=False)
-            indx = df_feats.index.to_list()
-            if 'model' in indx:
-                indx.remove('model')
-            if y_col in indx:
-                indx.remove(y_col)
-            # st.write(indx)
-            df_feats = df_feats.loc[indx]
-            weights = df_feats.values
-            V = []
-            max_cols = 5
-            col0, col1, col2, col3, col4 = st.columns(max_cols)
-            col_index = 0
-            # st.write(df_feats)
-            new_data = pd.DataFrame(columns=X.columns, index=[0])
-            # new_data.columns = X.columns
-            for i in range( df_feats.shape[0]):
-                # t = type(df_feats.iloc[i].values[0])
-                # st.write(i, col_index)
-                try: 
-                    df_feats.iloc[i] == float(df_feats.iloc[i])
-                    if col_index == 0:
-                        v = col0.number_input(f'enter {df_feats.index[i]}', value=df[df_feats.index[i]].mean())
-                    if col_index == 1:
-                        v = col1.number_input(f'enter {df_feats.index[i]}', value=df[df_feats.index[i]].mean())
-                    if col_index == 2:
-                        v = col2.number_input(f'enter {df_feats.index[i]}', value=df[df_feats.index[i]].mean())
-                    if col_index == 3:
-                        v = col3.number_input(f'enter {df_feats.index[i]}', value=df[df_feats.index[i]].mean())
-                    if col_index == 4:
-                        v = col4.number_input(f'enter {df_feats.index[i]}', value=df[df_feats.index[i]].mean())
-                    new_data[df_feats.index[i]] = v
-                    col_index += 1 
-                    col_index = col_index % max_cols
-                    V.append(v)
-                except:
-                    st.text(f'{i} is a string')
-            new_data.fillna(0, inplace=True)
-            st.write(new_data)
-            out = 0
-            for i in range(len(V)):
-                out += V[i] * weights[i]
-            # st.write(V)
-            # st.write(weights)
-            out = regr.predict(new_data)
-            st.code(f'With these inputs:\n {y_col} = {out[0]}')
+            if r2 > 0.6:
+                # st.text('y_col')
+                df_corr = df.corr()
+                df_feats = pd.DataFrame(np.abs(df_corr[y_col]))
+                df_feats.sort_values(by=[y_col], inplace=True, ascending=False)
+                indx = df_feats.index.to_list()
+                if 'model' in indx:
+                    indx.remove('model')
+                if y_col in indx:
+                    indx.remove(y_col)
+                # st.write(indx)
+                df_feats = df_feats.loc[indx]
+                weights = df_feats.values
+                V = []
+                max_cols = 5
+                col0, col1, col2, col3, col4 = st.columns(max_cols)
+                col_index = 0
+                # st.write(df_feats)
+                new_data = pd.DataFrame(columns=X.columns, index=[0])
+                # new_data.columns = X.columns
+                for i in range( df_feats.shape[0]):
+                    # t = type(df_feats.iloc[i].values[0])
+                    # st.write(i, col_index)
+                    try: 
+                        df_feats.iloc[i] == float(df_feats.iloc[i])
+                        if col_index == 0:
+                            v = col0.number_input(f'enter {df_feats.index[i]}', value=df[df_feats.index[i]].mean())
+                        if col_index == 1:
+                            v = col1.number_input(f'enter {df_feats.index[i]}', value=df[df_feats.index[i]].mean())
+                        if col_index == 2:
+                            v = col2.number_input(f'enter {df_feats.index[i]}', value=df[df_feats.index[i]].mean())
+                        if col_index == 3:
+                            v = col3.number_input(f'enter {df_feats.index[i]}', value=df[df_feats.index[i]].mean())
+                        if col_index == 4:
+                            v = col4.number_input(f'enter {df_feats.index[i]}', value=df[df_feats.index[i]].mean())
+                        new_data[df_feats.index[i]] = v
+                        col_index += 1 
+                        col_index = col_index % max_cols
+                        V.append(v)
+                    except:
+                        st.text(f'{i} is a string')
+                new_data.fillna(0, inplace=True)
+                st.write(new_data)
+                out = 0
+                for i in range(len(V)):
+                    out += V[i] * weights[i]
+                # st.write(V)
+                # st.write(weights)
+                out = regr.predict(new_data)
+                st.code(f'With these inputs:\n {y_col} = {out[0]}')
+            else:
+                st.code(f'The model has an R2 score of {r2} which is not high enough to be able to answer this question \nThe R2 limit is 0.7')
+
         else:
             st.text('sdfsd')
         
