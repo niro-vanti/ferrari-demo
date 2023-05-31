61f814c227593b6b0c246c64b14e528f67f1a4b1
niro <niro@vanti-analytics.com>
2023-05-31 09:56:31 +0300

    adsfsdf

--- assets/specific_demos/diego.py
+++ assets/specific_demos/diego.py
@@ -25,9 +25,17 @@ def diego(diego_strem, stop_stream, files):
     df.index.name='Vendor Batch'
     # st.text(df.index)
     with st.expander('Data preview'):
+        data_source = st.radio('Select data source',['Use application data','Upload my own'])
+        if data_source == 'Use application data':
+            qqq = 1
+            # st.dataframe(df)
+            # st.code(f'# of joint files: 4\n# of records: {df.shape[0]}\n# of columns: {df.shape[1]}')
+        if data_source == 'Upload my own':
+            qq = st.file_uploader('Upload file',type='csv', accept_multiple_files=False)
+            if qq is not None:
+                df = pd.read_csv(qq, index_col=0)
         st.dataframe(df)
         st.code(f'# of joint files: 4\n# of records: {df.shape[0]}\n# of columns: {df.shape[1]}')
-
     with st.expander('Relationship exploration'):
         col_list = []
         for col in df.columns.to_list():
